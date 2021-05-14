89aebbf733fc52918a4284e479befc368d002447
Admin <Admin@DESKTOP-0D65LPP>
2021-05-14 11:44:39 +0530

    First changes..

---
+++ Riders2021/src/main/java/com/springGlow/Riders2021Application.java
@@ -0,0 +1,14 @@
+package com.springGlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Riders2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Riders2021Application.class, args);
+		System.out.println("Hello Joy Rider");
+	}
+
+}
---
+++ Riders2021/src/test/java/com/springGlow/Riders2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springGlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Riders2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
